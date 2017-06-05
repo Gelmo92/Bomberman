ea59c485d2a239e3249af0d02839c445e2aaad21
Gelmo92 <20013172@studenti.uniupo.it>
2017-06-05 18:50:21 +0200

    Rimessa classe Explosion

---
+++ Bomberman/src/game/Explosion.java
@@ -0,0 +1,84 @@
+package game;
+
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.Timer;
+
+public class Explosion extends Entity {
+
+	ArrayList<Point> propagation;
+	private int delay = 3000;  //milliseconds
+	
+	public Explosion(Point firstPosition, int explosionRate) {
+		propagation = new ArrayList<Point>();
+		propagation.add(firstPosition);
+		burn(firstPosition, explosionRate);
+		ActionListener taskPerformer = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setChanged();
+				notifyObservers();
+							}
+		  };
+		  Timer timer = new Timer(delay, taskPerformer);
+		  timer.setRepeats(false);
+		  timer.start();
+	}
+
+	private void burn(Point firstPosition, int explosionRate) {
+		move(explosionRate,1);
+		move(explosionRate,2);
+		move(explosionRate,3);
+		move(explosionRate,4);
+		
+	}
+
+	@Override
+	Point getPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void move(int movement, int direction) {
+		Point pos = new Point(propagation.get(0));
+		for(int i = 0; movement > i; i++) {
+			Point nextPos = new Point(pos);
+			switch(direction) {
+			case 1:
+				nextPos.x += MapView.cell;
+				break;
+			case 2:
+				nextPos.y += MapView.cell;
+				break;
+			case 3:
+				nextPos.x -= MapView.cell;
+				break;
+			case 4:
+				nextPos.y -= MapView.cell;
+				break;
+			}
+			if(Map.canMove(nextPos, 1)) {
+				propagation.add(nextPos);
+				pos = nextPos;
+			}
+			else {
+				if(Map.canDestroy(nextPos)) {
+					propagation.add(nextPos);
+				}
+				return;
+			}
+		}
+
+	}
+
+	@Override
+	Point destroy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
