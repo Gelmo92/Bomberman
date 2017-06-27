5017655f8366d90bb92d1eec3ebc0bdae543c2c9
Jascar87 <riccardopidello@gmail.com>
2017-06-27 17:42:14 +0200

    Java doc Chest

--- Bomberman/src/game/Chest.java
+++ Bomberman/src/game/Chest.java
@@ -1,15 +1,26 @@
 package game;
 
 import java.awt.Point;
-
+/**
+ *La classe Chest rappresenta gli scrigni che se vengono colpiti da una esplisione rilasciano un bonus
+ * 
+ * @author Yuri Gelmotto
+ * @author Riccardo Pidello
+ *
+ */
 public class Chest extends Entity {
 	
 	private Point position;
-	
+	/**
+	 * 
+	 * @param newPosition sono le coordinate dello scrigno
+	 */
 	public Chest(Point newPosition) {
 		this.position = newPosition;
 	}
-
+	/**
+	 * @return le coordinate dello scrigno 
+	 */
 	@Override
 	Point getPos() {
 		return position;
@@ -20,7 +31,10 @@ public class Chest extends Entity {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	/**
+	 * @return le coordinate dello scrigno 
+	 */
 	@Override
 	Point destroy() {
 		return position;
