9d19c94bb3c1d5f5c75ee6042507d72b9e762513
Jascar87 <riccardopidello@gmail.com>
2017-06-27 15:07:15 +0200

    Java doc su Wall

--- Bomberman/src/game/Wall.java
+++ Bomberman/src/game/Wall.java
@@ -1,19 +1,35 @@
 package game;
 
 import java.awt.Point;
-
+/**
+ * La classe Wall rappresenta il muro nella mappa.
+ * Gli oggetti Wall possono essere o muri di perimetro che delimitano l'area di gioco
+ * o posso essere muri interni se sono interni possono essere o distruttibili o non 
+ * distruttibili.
+ * 
+ * @author Yuri Gelmotto
+ * @author Riccardo Pidello
+ */
 class Wall extends Entity {
 	
 	private Point position;
 	boolean destroyable;
 	public boolean perimetry;
 	
+	/**
+	 * 
+	 * @param firstPosition sono le coordinate del muro
+	 * @param destroyable definisce se il muro è distruttibile
+	 * @param perimetry definisce se il muro è distruttibile
+	 */
 	public Wall(Point firstPosition, boolean destroyable, boolean perimetry) {
 		position = firstPosition;
 		this.destroyable = destroyable;
 		this.perimetry = perimetry;
 	}
-
+	/**
+	 * @return le cooridnate del muro
+	 */
 	@Override
 	Point getPos() {
 		// TODO Auto-generated method stub
@@ -25,7 +41,11 @@ class Wall extends Entity {
 		// TODO Auto-generated method stub
 		
 	}
-	
+	/**
+	 * Il metodo va a rimuovere il muro dagli oggetti osservati
+	 * 
+	 * @return le coordiante del muro
+	 */
 	@Override
 	Point destroy() {
 		deleteObservers();
